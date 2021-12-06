55f801f6083606a3f7dbb7c952dbd967c6e4dc27
Petrusevski <angelpetrusevski@gmail.com>
2021-12-06 02:10:39 +0100

    index page refactor and styling

--- domasna2/prototype/src/main/java/com/dians/prototype/repository/FastFoodRepository.java
+++ domasna2/prototype/src/main/java/com/dians/prototype/repository/FastFoodRepository.java
@@ -1,6 +1,7 @@
 package com.dians.prototype.repository;
 
 import com.dians.prototype.model.FastFood;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
