86e334f12c7fef7ef37542ec13fae12b541f11ce
agonosmani <agon_osmani@live.com>
2021-12-06 21:48:40 +0100

    - Delete Target Folder

--- domasna2/prototype/src/main/java/com/dians/prototype/PrototypeApplication.java
+++ domasna2/prototype/src/main/java/com/dians/prototype/PrototypeApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class    PrototypeApplication {
+public class PrototypeApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrototypeApplication.class, args);
