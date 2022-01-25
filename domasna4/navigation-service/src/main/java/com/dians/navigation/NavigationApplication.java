d8e190321898e206f56af1c8927fe42aae8812ac
agonosmani <agon_osmani@live.com>
2022-01-25 18:32:29 +0100

    - Add Spring Security

--- domasna4/navigation-service/src/main/java/com/dians/navigation/NavigationApplication.java
+++ domasna4/navigation-service/src/main/java/com/dians/navigation/NavigationApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.client.RestTemplate;
 
 @ServletComponentScan
@@ -19,5 +21,10 @@ public class NavigationApplication {
     @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(10);
     }
 }
