26257d6e6140ecb5bb9ebf59f790766f6d244ff9
agonosmani <agon_osmani@live.com>
2022-01-26 16:38:52 +0100

    - Fix Assets

--- domasna4/navigation-service/src/main/java/com/dians/navigation/config/WebSecurityConfig.java
+++ domasna4/navigation-service/src/main/java/com/dians/navigation/config/WebSecurityConfig.java
@@ -23,7 +23,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeRequests()
-                .antMatchers("/").permitAll()
+                .antMatchers("/**").permitAll()
                 .antMatchers("/admin/**").hasRole("ADMIN")
                 .anyRequest().authenticated()
                 .and()
